0d45f0a5f6ada98bf9c283b0a64e82c2a0da61d5
samyogita <bhandarisamyogita@gmail.com>
2022-09-21 20:44:19 +0545

    constructor __init__

--- oops.py
+++ oops.py
@@ -1,16 +1,25 @@
 class Item:
-    def calculate_total_price(self, price, quantity):
-        return price * quantity
+
+    pay_rate = 0.8 #The pay rate after 20% discount
+    def __init__(self, name:str, price:float, quantity=0):
+        
+        # Run validation
+        assert price >=0, f"Price {price} is not greater than or equal to zero"
+        assert quantity >=0, f"Quantity {quantity} is not greater than or equal to zero"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+
+        
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item()
-item1.name = 'Phone'
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
-
-item2 = Item()
-item2.name = 'Laptop'
-item2.price = 1000
-item2.quantity = 3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+item1 = Item('Phone', 100, 5)
+item2 = Item('Laptop', 1000, 3)
+print(Item.pay_rate)
+print(item1.pay_rate)
+print(item2.pay_rate)
