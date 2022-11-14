14bb4e399cf55b13075992c256cc7ac301f49324
samyogita <bhandarisamyogita@gmail.com>
2022-11-14 17:09:32 +0545

    usage of __dict__

--- oops.py
+++ oops.py
@@ -3,7 +3,7 @@ class Item:
     pay_rate = 0.8 #The pay rate after 20% discount
     def __init__(self, name:str, price:float, quantity=0):
         
-        # Run validation
+        # Run validation to received arguements
         assert price >=0, f"Price {price} is not greater than or equal to zero"
         assert quantity >=0, f"Quantity {quantity} is not greater than or equal to zero"
 
@@ -20,6 +20,7 @@ class Item:
 
 item1 = Item('Phone', 100, 5)
 item2 = Item('Laptop', 1000, 3)
-print(Item.pay_rate)
-print(item1.pay_rate)
-print(item2.pay_rate)
+
+print(Item.__dict__) # All the attributes for class level are mapped in the dictionary and displayed
+print(item1.__dict__) # All the attributes for instance level
+
