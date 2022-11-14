66bee554ab2347f3ebf33e55ba33c665208549d2
samyogita <bhandarisamyogita@gmail.com>
2022-11-14 17:25:56 +0545

    adding items to list and displaying it using loop

--- oops.py
+++ oops.py
@@ -1,6 +1,7 @@
 class Item:
 
     pay_rate = 0.8 #The pay rate after 20% discount
+    all = []
     def __init__(self, name:str, price:float, quantity=0):
         
         # Run validation to received arguements
@@ -11,16 +12,29 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
         
 
         
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item('Phone', 100, 5)
+
+item1 = Item('Phone', 100, 1)
 item2 = Item('Laptop', 1000, 3)
+item3 = Item('Cable', 10, 5)
+item4 = Item('Mouse', 50, 5)
+item5 = Item('Keyboard', 75, 5)
 
-print(Item.__dict__) # All the attributes for class level are mapped in the dictionary and displayed
-print(item1.__dict__) # All the attributes for instance level
+# printing the name of available items in the list
+for instance in Item.all:
+    print(instance.name)
+
+
+
 
